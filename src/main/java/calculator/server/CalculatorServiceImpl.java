f8120a936c6403e3e64a6bbfe54b642664555748
phamtho123 <thopham.21082001@gmail.com>
2024-04-27 23:49:14 +0700

    fix conflict

--- src/main/java/calculator/server/CalculatorServiceImpl.java
+++ src/main/java/calculator/server/CalculatorServiceImpl.java
@@ -1,10 +1,10 @@
 package calculator.server;
 
-
 import com.proto.calculator.*;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 
-public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServiceImplBase {
+public final class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServiceImplBase {
     @Override
     public void sum(SumRequest request, StreamObserver<SumResponse> responseObserver) {
         responseObserver.onNext(SumResponse.newBuilder().setResult(
@@ -17,14 +17,16 @@ public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServi
     public void primes(PrimeRequest request, StreamObserver<PrimeResponse> responseObserver) {
         int number = request.getNumber();
         int divisor = 2;
+
         while (number > 1) {
             if (number % divisor == 0) {
-                number = number / divisor;
+                number /= divisor;
                 responseObserver.onNext(PrimeResponse.newBuilder().setPrimeFactor(divisor).build());
             } else {
                 ++divisor;
             }
         }
+
         responseObserver.onCompleted();
     }
 
@@ -33,6 +35,7 @@ public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServi
         return new StreamObserver<AvgRequest>() {
             int sum = 0;
             int count = 0;
+
             @Override
             public void onNext(AvgRequest value) {
                 sum += value.getNumber();
@@ -52,5 +55,48 @@ public class CalculatorServiceImpl extends CalculatorServiceGrpc.CalculatorServi
                 responseObserver.onCompleted();
             }
         };
+    }
+
+    @Override
+    public StreamObserver<MaxRequest> max(StreamObserver<MaxResponse> responseObserver) {
+        return new StreamObserver<MaxRequest>() {
+            int max = 0;
+
+            @Override
+            public void onNext(MaxRequest value) {
+                if (value.getNumber() > max) {
+                    max = value.getNumber();
+                    responseObserver.onNext(MaxResponse.newBuilder().setMax(max).build());
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                responseObserver.onError(t);
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+        };
+    }
+
+    @Override
+    public void sqrt(SqrtRequest request, StreamObserver<SqrtResponse> responseObserver) {
+        int number = request.getNumber();
+
+        if (number < 0) {
+            responseObserver.onError(Status.INVALID_ARGUMENT
+                    .withDescription("The number being sent cannot be negative")
+                    .augmentDescription("Number: " + number)
+                    .asRuntimeException());
+            return;
+        }
+
+        responseObserver.onNext(
+            SqrtResponse.newBuilder().setResult(Math.sqrt(number)).build()
+        );
+        responseObserver.onCompleted();
     }
 }
